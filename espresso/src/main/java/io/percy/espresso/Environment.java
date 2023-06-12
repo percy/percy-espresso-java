36993fd73c1ffe1a8fdf1acfebb97a09b2ebe090
Pankaj Yadav <pankajofficial443@gmail.com>
2023-06-12 18:19:48 +0530

    Version bump to 1.0.1 (#4)
    * Version bump
    
    * Version bump

--- espresso/src/main/java/io/percy/espresso/Environment.java
+++ espresso/src/main/java/io/percy/espresso/Environment.java
@@ -1,7 +1,7 @@
 package io.percy.espresso;
 
 public class Environment {
-    public static final String SDK_VERSION = "1.0.0";
+    public static final String SDK_VERSION = "1.0.1";
     private static final String SDK_NAME = "espresso-java";
 
     public String getClientInfo() {
